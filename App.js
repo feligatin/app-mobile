7f26dbc116796d66e349a2e715f2f621198ef67f
Felipe Serrano <feligatin@gmail.com>
2025-02-26 14:36:26 -0500

    remove unused app.js

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { ModalPortal } from "react-native-modals";
 import { Provider } from "react-redux";
 import StackNavigator from "./StackNavigator";
 import store from "./store";
 
-
 export default function App() {
   return (
-    <>
-      <Provider store={store}>
-        <StackNavigator />
-        <ModalPortal />
-      </Provider>
-    </>
+    <Provider store={store}>
+      <StackNavigator />
+      <ModalPortal />
+    </Provider>
   );
 }
 
